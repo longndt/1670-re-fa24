6fcd501d50b317d014ecf7e90992ee25d8047d7a
Nguyen Dinh Tran Long <longndt226@gmail.com>
2024-09-25 14:28:02 +0700

    lesson2

---
+++ Lesson02/Web01/Controllers/AppleController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web01.Controllers
+{
+    public class AppleController : Controller
+    {
+        //localhost:PORT/ControllerName/ActionName
+        //localhost:PORT/Apple/Index
+        //localhost:PORT/Apple
+        [Route("/")]
+        public IActionResult Index()
+        {
+            //Views/ControllerName/ActionName.cshtml
+            //Views/Apple/Index.cshtml
+            return View();
+        }
+
+        //localhost:PORT/Apple
+        public IActionResult Iphone()
+        {
+            return View();
+        }
+
+        public IActionResult Ipad()
+        {
+            return View();
+        }
+
+        public IActionResult Macbook()
+        {
+            return View();
+        }
+    }
+}
