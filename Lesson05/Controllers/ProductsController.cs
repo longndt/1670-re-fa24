2ff56cb15be57f750ca8b1cd89c9ffe3b2cd71cb
Nguyen Dinh Tran Long <longndt226@gmail.com>
2024-10-04 16:29:58 +0700

    lesson5

---
+++ Lesson05/Controllers/ProductsController.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Lesson05.Data;
+using Lesson05.Models;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
+using static Microsoft.Extensions.Logging.EventSource.LoggingEventSource;
+using System.IO;
+using Microsoft.AspNetCore.Http;
+
+namespace Lesson05.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly Lesson05Context _context;
+
+        public ProductsController(Lesson05Context context)
+        {
+            _context = context;
+        }
+
+        // GET: Products
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Product.ToListAsync());
+        }
+
+        // GET: Products/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Product
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+
+        // GET: Products/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Products/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Product product, IFormFile Image)
+        {
+            if (ModelState.IsValid)
+            {
+                //check if a new image file is uploaded or not
+                if (Image != null && Image.Length > 0)
+                {
+                    //set image file name
+                    //Note: should add a prefix to make sure the file name is unique
+                    var fileName = Guid.NewGuid().ToString() + "_" + Path.GetFileName(Image.FileName);
+                    //set image file location
+                    //Note: should create a subfolder named "images" in "wwwroot" to store all images
+                    var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\images", fileName);
+
+                    using (var stream = new FileStream(filePath, FileMode.Create))
+                    {
+                        //copy (upload) image file from orignal location to project folder
+                        Image.CopyTo(stream);
+                    }
+
+                    //set image file name for book cover
+                    product.Image = "/images/" + fileName;
+                }
+                _context.Product.Add(product);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(product);
+        }
+
+        // GET: Products/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Product.FindAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+
+        // POST: Products/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Image,Price")] Product product)
+        {
+            if (id != product.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(product);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ProductExists(product.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(product);
+        }
+
+        // GET: Products/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Product
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+
+        // POST: Products/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var product = await _context.Product.FindAsync(id);
+            _context.Product.Remove(product);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ProductExists(int id)
+        {
+            return _context.Product.Any(e => e.Id == id);
+        }
+
+        //search function
+        public IActionResult SearchProduct(string keyword)
+        {
+            var products = _context.Product.Where(p => p.Name.Contains(keyword)).ToList();
+            return View("Index", products);
+        }
+
+        //sort functions
+        public IActionResult SortByPriceAsc()
+        {
+            var products = _context.Product.OrderBy(p => p.Price).ToList();
+            return View("Index", products);
+        }
+
+        public IActionResult SortByPriceDesc()
+        {
+            var products = _context.Product.OrderByDescending(p => p.Price).ToList();
+            return View("Index", products);
+        }
+    }
+}
---
+++ Lesson05/Data/Lesson05Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Lesson05.Models;
+
+namespace Lesson05.Data
+{
+    public class Lesson05Context : DbContext
+    {
+        public Lesson05Context (DbContextOptions<Lesson05Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Lesson05.Models.Product> Product { get; set; }
+    }
+}
